b86340164ae0d9f17dd37229cc5243de9be13fbe
MdTasib <ohidulalam442@gmail.com>
2021-06-09 13:56:47 +0600

    portfolio start

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,11 @@
+import './intro.scss';
+
+function Intro() {
+    return (
+        <div className='intro'>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+    );
+}
+
+export default Intro;
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,11 @@
+import './portfolio.scss';
+
+function Portfolio() {
+    return (
+        <div className='portfolio'>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+    );
+}
+
+export default Portfolio;
