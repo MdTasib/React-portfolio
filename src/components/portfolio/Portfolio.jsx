68252947b4291193338d2b2c6acda6b2504c8b47
MdTasib <ohidulalam442@gmail.com>
2021-06-10 18:33:55 +0600

    intro page done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,13 @@ import './portfolio.scss';
 function Portfolio() {
     return (
         <div className='portfolio' id='portfolio'>
-            <p>Lorem ipsum dolor sit amet.</p>
+            <h1>Portfolio</h1>
+            <ul>
+                <li>Awesome</li>
+                <li>MERN Stack</li>
+                <li>React Js</li>
+                <li>Javascript</li>
+            </ul>
         </div>
     );
 }
