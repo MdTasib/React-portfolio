c539a77cd24ddd8e112c2996566349c73b4e01c2
MdTasib <ohidulalam442@gmail.com>
2021-06-10 19:04:35 +0600

    portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,15 +1,42 @@
 import './portfolio.scss';
+import burjAlArab from '../../images/bruj-al-arab.png';
 
 function Portfolio() {
     return (
         <div className='portfolio' id='portfolio'>
             <h1>Portfolio</h1>
             <ul>
-                <li>Awesome</li>
+                <li className='active'>Awesome</li>
                 <li>MERN Stack</li>
                 <li>React Js</li>
                 <li>Javascript</li>
             </ul>
+            <div className="container">
+                <div className="item">
+                    <img src={burjAlArab} alt="Burj Al Arab" />
+                    <h3>Burj Al Arab</h3>
+                </div>
+                <div className="item">
+                    <img src={burjAlArab} alt="Burj Al Arab" />
+                    <h3>Burj Al Arab</h3>
+                </div>
+                <div className="item">
+                    <img src={burjAlArab} alt="Burj Al Arab" />
+                    <h3>Burj Al Arab</h3>
+                </div>
+                <div className="item">
+                    <img src={burjAlArab} alt="Burj Al Arab" />
+                    <h3>Burj Al Arab</h3>
+                </div>
+                <div className="item">
+                    <img src={burjAlArab} alt="Burj Al Arab" />
+                    <h3>Burj Al Arab</h3>
+                </div>
+                <div className="item">
+                    <img src={burjAlArab} alt="Burj Al Arab" />
+                    <h3>Burj Al Arab</h3>
+                </div>
+            </div>
         </div>
     );
 }
