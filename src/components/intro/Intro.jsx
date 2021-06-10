98852bcdd6d5dc51ebe9eee7f6b1be094cb0a755
MdTasib <ohidulalam442@gmail.com>
2021-06-10 18:24:50 +0600

    intro page done

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,39 @@
 import './intro.scss';
+import me from '../../images/me2.png';
+import arrowDown from '../../images/down.png';
+import { init } from 'ityped';
+import { useRef, useEffect } from 'react';
 
 function Intro() {
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ['Front End Developer', 'React.js Developer', 'JavaScript Developer']
+        })
+    }, []);
+
+
     return (
         <div className='intro' id='intro'>
-            <p>Lorem ipsum dolor sit amet.</p>
+            <div className="left">
+                <div className="imgContainer">
+                    <img src={me} alt="mohammad tasib" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There! I'm</h2>
+                    <h1>Mohammad Tasib</h1>
+                    <h3>A Professional <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src={arrowDown} alt="" />
+                </a>
+            </div>
         </div>
     );
 }
