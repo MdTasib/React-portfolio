3fff24be6bd9378e6503c0b8021b65386ef06fb6
MdTasib <ohidulalam442@gmail.com>
2021-06-11 22:42:14 +0600

    slider area done

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,61 +1,73 @@
 import './works.scss';
 import mobile from '../../images/mobile.png';
-import slider1 from '../../images/slider1.png';
+import landingpage from '../../images/slider1.png';
 import slider2 from '../../images/slider2.png';
-import slider3 from '../../images/slider3.png';
 import slider4 from '../../images/slider4.png';
 import arrow from '../../images/arrow.png';
 import glope from '../../images/globe.png';
 import writing from '../../images/writing.png';
+import { useState } from 'react';
 
 function Works() {
+    const [currentSlide, setCurrentSlide] = useState(0);
+
     const data = [
         {
             id: 1,
             icon: glope,
-            title: 'Awesome Desing',
+            title: 'Awesome Landing Page',
             desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, eveniet.',
-            img: slider1
+            img: landingpage
         },
         {
             id: 2,
             icon: mobile,
             title: 'One Page Application',
-            desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, eveniet.',
+            desc: 'Quis est pariatur quidem voluptatibus sunt minima nulla vel animi',
             img: slider2
         },
         {
             id: 3,
-            icon: mobile,
-            title: 'Dynamic Application',
-            desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, eveniet.',
-            img: slider3
+            icon: writing,
+            title: 'Full Stack Application',
+            desc: ' Ratione amet tempora officiis quidem! Pariatur error beatae id hic itaque nostrum nesciunt eligendi sed neque?',
+            img: slider4
         }
     ]
 
+    const handleClick = (way) => {
+        way === 'left'
+            ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+            : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0)
+    }
+
     return (
         <div className='works' id='works'>
-            <div className="slider">
-                <div className="container">
-                    <div className="item">
-                        <div className="left">
-                            <div className="leftContainer">
-                                <div className="imgContainer">
-                                    <img src={mobile} alt="" />
+            <div className="slider" style={{ transform: `translateX(-${currentSlide * 100}vw)` }}>
+                {
+                    data.map(d => (
+                        <div className="container">
+                            <div className="item">
+                                <div className="left">
+                                    <div className="leftContainer">
+                                        <div className="imgContainer">
+                                            <img src={d.icon} alt="" />
+                                        </div>
+                                        <h2>{d.title}</h2>
+                                        <p>{d.desc}</p>
+                                        <span>Projects</span>
+                                    </div>
                                 </div>
-                                <h2>Title</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, eveniet.</p>
-                                <span>Projects</span>
+                                <div className="right">
+                                    <img src={d.img} alt="" />
+                                </div>
                             </div>
                         </div>
-                        <div className="right">
-                            <img src={slider1} alt="" />
-                        </div>
-                    </div>
-                </div>
+                    ))
+                }
             </div>
-            <img src={arrow} alt="" className='arrow left' />
-            <img src={arrow} alt="" className='arrow right' />
+            <img src={arrow} alt="" className='arrow left' onClick={() => { handleClick('left') }} />
+            <img src={arrow} alt="" className='arrow right' onClick={() => { handleClick() }} />
         </div>
     );
 }
