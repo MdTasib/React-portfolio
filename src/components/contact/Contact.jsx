7f631e4d109246045571e42f5aba608219ec6f13
MdTasib <ohidulalam442@gmail.com>
2021-06-12 14:03:22 +0600

    project complete

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,31 @@
 import './contact.scss';
+import shake from '../../images/shake.svg';
+import { useState } from 'react';
 
 function Contact() {
+    const [message, setMessage] = useState(false);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setMessage(true);
+    }
+
     return (
         <div className='contact' id='contact'>
-
+            <div className="left">
+                <img src={shake} alt="" />
+            </div>
+            <div className="right">
+                <h2>Contact</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder='Your Email' />
+                    <textarea placeholder='Your Message'></textarea>
+                    <button type="submit">Send</button>
+                    {
+                        message && <span>Thanks, I'll reply ASAP :)</span>
+                    }
+                </form>
+            </div>
         </div>
     );
 }
