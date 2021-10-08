e69680f6cb660956a01ed6efda7900e6ec65d641
MdTasib <ohidulalam442@gmail.com>
2021-10-08 22:35:58 +0600

    email send functionality done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,34 +1,54 @@
 import './contact.scss';
 import shake from '../../images/shake.svg';
 import { useState } from 'react';
+import emailjs from 'emailjs-com';
 
 function Contact() {
-    const [message, setMessage] = useState(false);
+	const [message, setMessage] = useState(false);
 
-    const sendEmail = e => {
-        e.preventDefault();
-        setMessage(true);
-    }
+	const sendEmail = e => {
+		e.preventDefault();
 
-    return (
-        <div className='contact' id='contact'>
-            <div className="left">
-                <img src={shake} alt="" />
-            </div>
-            <div className="right">
-                <h2>Contact</h2>
-                <form onSubmit={sendEmail}>
-                    <input type="text" placeholder='Your Name' name='name' required />
-                    <input type="email" placeholder='Your Email' name='email' required />
-                    <textarea placeholder='Your Message' name='message'></textarea>
-                    <button type="submit">Send</button>
-                    {
-                        message && <span>Not working. But Coming soon</span>
-                    }
-                </form>
-            </div>
-        </div>
-    );
+		emailjs
+			.sendForm(
+				'service_97v1wkb',
+				'template_42wy8ps',
+				e.target,
+				'user_y8p4lPQmmLkW31uRt8aGx'
+			)
+			.then(
+				result => {
+					if (result.text) {
+						setMessage(true);
+					}
+				},
+				error => {
+					setMessage(error.message);
+				}
+			);
+	};
+
+	return (
+		<div className='contact' id='contact'>
+			<div className='left'>
+				<img src={shake} alt='' />
+			</div>
+			<div className='right'>
+				<h2>Contact</h2>
+				<form onSubmit={sendEmail}>
+					<input type='text' placeholder='Your Name' name='name' required />
+					<input type='email' placeholder='Your Email' name='email' required />
+					<textarea placeholder='Your Message' name='message'></textarea>
+					<button type='submit'>Send</button>
+					{message ? (
+						<span className='success'>Email Send Successfully</span>
+					) : (
+						<span className='error'>{message}</span>
+					)}
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default Contact;
