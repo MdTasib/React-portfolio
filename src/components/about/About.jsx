8b09e56b8c9d948f740d3b72b3847285a5ebee07
MdTasib <ohidulalam442@gmail.com>
2021-11-29 17:12:28 +0600

    edit tanvir

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,25 +1,38 @@
+/* eslint-disable no-unused-vars */
 import './about.scss';
 import me from '../../images/me3.png';
 
 function About() {
-    return (
-        <div className='about' id='about'>
-            <div className="myself">
-                <div className="left">
-                    <h4>INTRODUCE MYSELF</h4>
-                    <p>
-                        I have professional experience with programming languages and tools such as React, Redux, HTML, CSS, Sass, Bootstrap, javascript, ES6, Express Js, Material UI, and Firebase Auth to contribute features by writing and maintaining code. I also have experience in Photoshop, Node.js, JSON, WordPress, After Effects. I am highly motivated and enthusiastic, always with excellent attention to detail. I will give my best effort to achieve the best result. All tasks I try to carry out on time and in full. So if you have any questions or inputs, please don't hesitate to contact me.
-                    </p>
-                    <a href="https://docs.google.com/document/d/10t5yWxzV1vWXKNVcbyB7qBe0ZxqJAakGitGOk-9K8Fo/edit?usp=sharing">
-                        <button>GET RESUME</button>
-                    </a>
-                </div>
-                <div className="right">
-                    <img src={me} alt="" />
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className='about' id='about'>
+			<div className='myself'>
+				<div className='left'>
+					<h4>INTRODUCE MYSELF</h4>
+					<p>
+						I have professional experience with programming languages and tools
+						such as React, Redux, HTML, CSS, Sass, Bootstrap, javascript, ES6,
+						Express Js, Material UI, and Firebase Auth to contribute features by
+						writing and maintaining code. I also have experience in Photoshop,
+						Node.js, JSON, WordPress, After Effects. I am highly motivated and
+						enthusiastic, always with excellent attention to detail. I will give
+						my best effort to achieve the best result. All tasks I try to carry
+						out on time and in full. So if you have any questions or inputs,
+						please don't hesitate to contact me.
+					</p>
+					<a href='https://docs.google.com/document/d/10t5yWxzV1vWXKNVcbyB7qBe0ZxqJAakGitGOk-9K8Fo/edit?usp=sharing'>
+						<button>GET RESUME</button>
+					</a>
+				</div>
+				<div className='right'>
+					{/* <img src={me} alt="" /> */}
+					<img
+						src='https://lh3.googleusercontent.com/proxy/Hgx8y931zlDA0MOaciRtxGY2zUwBiEoZl-YOoYbj5lwc4DZg53EQ9WJK6UOYLBmDp0MFa_ZSJVQKQRGUAjaTJFupb3TVRYuQxUQW-goh--Y08SA'
+						alt='mohammad tasib'
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default About;
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import './intro.scss';
 import me from '../../images/me2.png';
 import arrowDown from '../../images/down.png';
@@ -5,37 +6,47 @@ import { init } from 'ityped';
 import { useRef, useEffect } from 'react';
 
 function Intro() {
-    const textRef = useRef();
+	const textRef = useRef();
 
-    useEffect(() => {
-        init(textRef.current, {
-            showCursor: true,
-            backDelay: 1500,
-            backSpeed: 60,
-            strings: ['Front End Developer', 'React.js Developer', 'JavaScript Developer']
-        })
-    }, []);
+	useEffect(() => {
+		init(textRef.current, {
+			showCursor: true,
+			backDelay: 1500,
+			backSpeed: 60,
+			strings: [
+				'Front End Developer',
+				'React.js Developer',
+				'JavaScript Developer',
+			],
+		});
+	}, []);
 
-
-    return (
-        <div className='intro' id='intro'>
-            <div className="left">
-                <div className="imgContainer">
-                    <img src={me} alt="mohammad tasib" />
-                </div>
-            </div>
-            <div className="right">
-                <div className="wrapper">
-                    <h2>Hi There! I'm</h2>
-                    <h1>Mohammad Tasib</h1>
-                    <h3>A Professional <span ref={textRef}></span></h3>
-                </div>
-                <a href="#about">
-                    <img src={arrowDown} alt="" />
-                </a>
-            </div>
-        </div>
-    );
+	return (
+		<div className='intro' id='intro'>
+			<div className='left'>
+				<div className='imgContainer'>
+					{/* <img src={me} alt='mohammad tasib' /> */}
+					<img
+						src='https://www.pngall.com/wp-content/uploads/2016/05/Man-Download-PNG.png'
+						alt='mohammad tasib'
+					/>
+				</div>
+			</div>
+			<div className='right'>
+				<div className='wrapper'>
+					<h2>Hi There! I'm</h2>
+					{/* <h1>Mohammad Tasib</h1> */}
+					<h1>Mohammad Tanvir</h1>
+					<h3>
+						A Professional <span ref={textRef}></span>
+					</h3>
+				</div>
+				<a href='#about'>
+					<img src={arrowDown} alt='' />
+				</a>
+			</div>
+		</div>
+	);
 }
 
 export default Intro;
